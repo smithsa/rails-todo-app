5ba306b9744cd9421e8bbf167d3b9b72c1d27344
Sade Smith <smithsade13@gmail.com>
2019-06-26 17:19:05 -0500

    add functionality to complete a todo item

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   root "todo_lists#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :todo_lists do
-  resources :todo_items
-end
-  
+  resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
+  end
+
 end

8d8d3578ffa0bf5467d85e8459044a09f7c3f329
Sade Smith <smithsade13@gmail.com>
2019-06-26 15:04:55 -0500

    add scafoldding for todo lists and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root "todo_lists#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :todo_lists do
+  resources :todo_items
+end
+  
 end
